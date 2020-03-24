a9eceea8e246b5f72d80f351e5262683ba8748c4
Eduardo de Rivero Manrique <ederiveroman@gmail.com>
2020-03-24 17:35:23 -0500

    avance

--- Backend/VirtualBack/Dia1/09-condicionales.py
+++ Backend/VirtualBack/Dia1/09-condicionales.py
@@ -12,11 +12,61 @@ edad = 15
 
 # elif => verifica otra condicion si puede ser verdad, si o si tiene que ir antes del else
 # edad = 15
-if edad >= 65:
-    print("Puede votar pero es opcional")
-elif edad >= 18:
-    print("Es obligatorio votar")
-else:
-    print("No puede votar")
+# if edad >= 65:
+#     print("Puede votar pero es opcional")
+# elif edad >= 18:
+#     print("Es obligatorio votar")
+# else:
+#     print("No puede votar")
 
-# Ingresar por teclado el sexo de la persona si ingresa Masculino que indique que es hombre, si ingresa Femenino que indique es es mujer y si ingresa otro sexo que indique que aun no esta registrado 
+# Ingresar por teclado el sexo de la persona si ingresa Masculino que indique que es hombre, si ingresa Femenino que indique es es mujer y si ingresa otro sexo que indique que aun no esta registrado
+# sexo = input("Ingresa tu sexo:")
+# if sexo == "Masculino" or sexo=="masculino":
+#     print("Eres hombre")
+# elif sexo == "Femenino" or sexo=="femenino":
+#     print("Eres mujer")
+# else:
+#     print("Aun no tenemos registrado ese sexo")
+
+# BUCLES
+# FOR
+# Es usado para iterar sobre una secuencia de elementos
+texto = "Buenas tardes"
+# for letra in texto:
+#     print(letra, end=" ")
+
+# range() => admite hasta 3 parametros, si le ingreso un parametro es el tope, si le ingreso dos, el primero es el inicio y el segundo es el tope, si le ingreso tres parametros, el primero es el inicio, el segundo es el tope y el tercero es cuantas veces va a modificar en cada vuelta
+# for numero in range(5, 15):
+#     print(numero)
+#     if numero == 10:
+#         break
+#     if numero == 8:
+#         continue
+#     print("Otra letra")
+# break => para el bucle
+# continue => para SOLAMENTE la iteracion actual
+
+# Ingresar 10 numeros y guardarlos en una lista y luego indicar cuales son pares y cuales son impares
+# Facil: Teniendo 10 numeros indicar cuales son pares y cuales son impares
+numeros = [2, 6, 9, 15, 18, 19, 23, 43, 68, 14]
+lista_numeros=[]
+for numero in range(10):
+    dato = input(f"Ingresa el numero {numero+1}: ")
+    dato = int(dato)
+    lista_numeros.append(dato)
+print(lista_numeros)
+numeros_pares=[]
+numeros_impares=[]
+for numero in lista_numeros:
+    if numero%2==0:
+        numeros_pares.append(numero)
+    else:
+        numeros_impares.append(numero)
+print(f"Los numeros pares son: {numeros_pares}")
+print("Los numeros impares son: {}".format(numeros_impares))
+
+
+# se desea saber cuantos son pares y cuantos son impares
+numeros = [2, 6, 9, 15, 18, 19, 23, 43, 68, 14]
+# hay 4 pares
+# hay 6 impares
