b9c24520a54d00980d5e556877862631bcca9c82
jmontes50 <osmarmontes50@gmail.com>
2020-04-07 12:21:32 -0500

    avance2

--- VirtualFront/Día6/01-ejercicioconclase2.js
+++ VirtualFront/Día6/01-ejercicioconclase2.js
@@ -4,8 +4,7 @@
  * 2. agregar sus datos y construirlo
  * 3. dentro de las propiedades que tenga Empresa tiene        que    tener un arreglo de Usuarios
  * 4. voy a necesitar objetos Usuarios
- * 5. Cada objeto Usuario dentro de ese arreglo tendrá sus propios
- *      datos
+ * 5. Cada objeto Usuario dentro de ese arreglo tendrá sus propios datos
  * 6. Cada Usuario tendrá un arreglo de mensajes
  * 7. tengo que hallar la manera de poder identificar al 
  *      usuario que quiera mandar el mensaje
@@ -28,7 +27,8 @@ class Empresa{
     agregarUsuario(){
         let usuarioTmp = {
             usu_nom:'',
-            usu_telf:''
+            usu_telf:'',
+            usu_msjs:[]
         }
         //capturo los datos para ingresar un usuario
         usuarioTmp.usu_nom = prompt("Ingrese el nombre del usuario");
@@ -37,7 +37,9 @@ class Empresa{
         let telf = usuarioTmp.usu_telf;
 
         for(let i = 0; i < this.emp_usu.length; i++){
+
             let iTelf = this.emp_usu[i].usu_telf;
+
             if(telf == iTelf){
                 console.log("oe el usuario ya existe");
                 //con el return termina la ejecución de este método
@@ -47,9 +49,57 @@ class Empresa{
         //agregar el usuario
         this.emp_usu.push(usuarioTmp);
     }
+
+    mandarMensaje(){
+        let destinatario = prompt("Ingresa el núm. de teléfono");
+
+        for(let i=0; i < this.emp_usu.length;i++){
+            if(destinatario == this.emp_usu[i].usu_telf){
+                //obtenemos el usuario para mandar el mensaje, agergaremos el mensaje
+                let mensaje = prompt(`Ingresa el mensaje para ${this.emp_usu[i].usu_nom}`);
+                //estamos agregando el mensage obtenido por el prompt en la propiedad usu_msjs
+                this.emp_usu[i].usu_msjs.push(mensaje);
+                //imprimirmos los mensajes del usuario
+                console.log(this.emp_usu[i].usu_msjs);
+            }else{
+                console.log("El telf. Ingresado no existe");
+            }
+        }
+    }
+
+    verMensajes(){
+        /**0. Opción 3
+         * 1. que el usuario ingrese su numero de telefono
+         * 2. segun el numero de telefono, me imprima sus mensajes
+         */
+    }
+
 }
 //fin de la clase Empresa
 
 let tecsup = new Empresa("Tecsup Arequipa","Av. Hartley");
 
-tecsup.agregarUsuario();
+do{
+    //aca adentro el codigo a ejecutar
+    opcion = +prompt(`
+                    :::::::::::Bienvenido:::::::::::
+                    Escoja entre las siguientes opciones
+                    1. Agregar un Usuario
+                    2. mandar un mensaje a un usuario
+                    4. Salir`);
+
+    switch(opcion){
+        case 1:
+            tecsup.agregarUsuario();
+            break;
+        case 2:
+            tecsup.mandarMensaje();
+            break;
+        case 4:
+            console.log("Bye, salio del sistema");
+            break;
+        default:
+            console.log("La opción ingresada no es valida");
+            break;
+    }
+}while(opcion != 4)
