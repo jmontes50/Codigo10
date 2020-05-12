b0373d3522d8a9fea6a213ba380c0fc8d3f876ed
Eduardo de Rivero Manrique <ederiveroman@gmail.com>
2020-05-12 18:02:45 -0500

    Dia 8

--- Backend/VirtualBack/Dia8/flaskRestful-con-bd.py
+++ Backend/VirtualBack/Dia8/flaskRestful-con-bd.py
@@ -138,7 +138,103 @@ class Categoria(Resource):
             'message':'Categoria eliminada con exito'
         }
 
+class Inquilino(Resource):
+    def post(self):
+        data = request.get_json()
+        if data.__contains__('nombre') and data.__contains__('fono') and data.__contains__('correo') and data.__contains__('cat_id'):
+            cur = mysql.connection.cursor()
+            cur.execute("SELECT * from T_CATEGORIA where cat_id =%s",(data['cat_id'],))
+            resultado = cur.fetchone()
+            if resultado:
+                cur.execute("INSERT INTO T_INQUILINO (inq_nombre, inq_fono, inq_correo, cat_id) VALUES (%s,%s,%s,%s)",(data['nombre'],data['fono'],data['correo'],data['cat_id']))
+                mysql.connection.commit()
+                cur.close()
+                return {
+                    'ok':True,
+                    'message':'Se agrego el inquilo con exito'
+                }, 201
+            else:
+                cur.close()
+                return {
+                    'ok':False,
+                    'message': 'La categoria no existe'
+                }, 404
+        else:
+            return {
+                'ok':False,
+                'message':'Faltan datos'
+            },500
+    def get(self):
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * from T_INQUILINO as Inq inner join T_categoria as Cat on Inq.cat_id = Cat.cat_id where inq_est=true")
+        # QUE lo que devuelva sea en forma de un diccionario (llave y un valor)
+        # Traer con su nombre de categoria mas 
+        data = cur.fetchall()
+        cur.close()
+        nuevaData=[]
+        for fila in data:
+            print(fila)
+            nuevaData.append({
+                'id':fila[0],
+                'nombre':fila[1],
+                'telefono':fila[2],
+                'correo':fila[3],
+                'categoria':{
+                    'id':fila[6],
+                    'nombre':fila[7]
+                }
+            })
+        # print(data)
+        return {
+            'ok':True,
+            'content':nuevaData
+        }
+    def delete(self, id_inquilino):
+        cur = mysql.connection.cursor()
+        cur.execute("UPDATE T_INQUILINO set inq_est=false where inq_id=%s",(id_inquilino,))
+        mysql.connection.commit()
+        cur.close()
+        return{
+            'ok':True,
+            'message':'Inquilino eliminado con exito'
+        }
 
+    def put(self, id_inquilino):
+        data = request.get_json()
+        if data.__contains__('nombre') and data.__contains__('fono') and data.__contains__('correo') and data.__contains__('cat_id'):
+            cur = mysql.connection.cursor()
+            cur.execute("SELECT * from T_CATEGORIA where cat_id =%s",(data['cat_id'],))
+            resultado = cur.fetchone()
+            cur.execute("SELECT * from t_inquilino where inq_id=%s and inq_est=true",(id_inquilino,))
+            resultado2 = cur.fetchone()
+            if resultado and resultado2:
+                cur.execute("UPDATE T_INQUILINO SET inq_nombre=%s, inq_fono=%s, inq_correo=%s, cat_id=%s where inq_id=%s",(data['nombre'],data['fono'],data['correo'],data['cat_id'], id_inquilino))
+                mysql.connection.commit()
+                cur.close()
+                return {
+                    'ok':True,
+                    'message':'Se modifico el inquilo con exito'
+                }, 201
+            else:
+                cur.close()
+                if resultado is None:
+                    return {
+                        'ok':False,
+                        'message': 'La categoria no existe'
+                    }, 404
+                elif resultado2 is None:
+                    return {
+                        'ok':False,
+                        'message': 'El inquilino no existe'
+                    }, 404
+        else:
+            return {
+                'ok':False,
+                'message':'Faltan datos'
+            },500
+
+
+api.add_resource(Inquilino,'/inquilino','/inquilino/<int:id_inquilino>')
 api.add_resource(Categoria,'/categoria','/categoria/<int:id_categoria>')
 
 if __name__ =="__main__":
